311a93a88b6a31e5a6036ae9f71771855a7a3c37
SivanPesahov <sivanp27540@gmail.com>
2024-10-28 13:32:07 +0200

    in-the-press

--- src/app/[locale]/(unauth)/about/company/page.tsx
+++ src/app/[locale]/(unauth)/about/company/page.tsx
@@ -1,9 +1,16 @@
 import FoundingTeam from '@/components/Founding-Team'
+import InThePress from '@/components/In-The-Press'
+
 import React from 'react'
+
+import pic2 from '../../../../../../public/assets/images/poster-images/2368685235d8ce4cf6be9c3140f3254e.jpeg'
 
 function Page() {
   return (
-    <FoundingTeam  />
+    <>
+      <FoundingTeam  />
+      <InThePress imageUrls={[pic2.src]}/>
+    </>
   )
 }
 
